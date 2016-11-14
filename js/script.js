3d26b23a7d8d98fbc735025e07396b2355fec9fe
Szymon <szymon.grotha@gmail.com>
2016-11-14 20:45:18 +0100

    Few Fixes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 $(function(){
-	var carouselList = $('carousel ul');
+	var carouselList = $('#carousel ul');
+	setInterval(changeSlide, 5000);
 
-	$(function changeSlide(){
-		carouselList.animate({'marginLeft':-400}, 500, move);
-			setInterval(changeSlide, 3000);
+	function changeSlide(){
+			carouselList.animate({'marginLeft':-600}, 500, move);
+	}
 
-	})
+	function changeSlideLeft(){
+		carouselList.animate({'marginLeft':+600}, 500, moveLeft);
+	}
 
-	$(function move(){
-		var firstItem = carouselList.find('li:fist');
+	function move(){
+		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
-
 		lastItem.after(firstItem)
 		carouselList.css({marginLeft:0});
+	}
 
+	function moveLeft(){
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+		firstItem.before(lastItem)
+		carouselList.css({marginLeft:0});
+	}
+
+	changeSlide()
+
+	$('.fa-chevron-right').on('click', function() {
+			changeSlide()
 	})
 
-
+	$('.fa-chevron-left').on('click', function() {
+			changeSlideLeft()
+	})
 });	
+

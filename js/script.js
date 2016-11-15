1c57634e7ca6fd0a0f3b526ad34e36830d98ee99
Kacper Grzeszczyk <kiper@desmart.com>
2016-11-15 18:20:04 +0100

    Fix carousel issues

--- js/script.js
+++ js/script.js
@@ -1,35 +1,66 @@
 $(function(){
 	var carouselList = $('#carousel ul');
-	setInterval(changeSlide, 5000);
+	var interval = setInterval(changeSlideRight, 4000);
+	carouselList.css({marginLeft:-600});
 
-	function changeSlide(){
-			carouselList.animate({'marginLeft':-600}, 500, move);
+	var i = 0;
+	checkPoint();
+
+	function checkPoint() {
+		if(i === - 1) {
+			i = 4;
+		} else if (i === 5) {
+			i = 0;
+		}
+
+		$('.points i').each(function( index ) {
+			if(i === index) {
+				$(this).removeClass('fa-circle-thin').addClass('fa-circle');
+			} else {
+				$(this).removeClass('fa-circle').addClass('fa-circle-thin');
+			}
+		});
+	}
+
+	function changeSlideRight(){
+		carouselList.animate({'marginLeft':-1200}, 500, moveRight);
 	}
 
 	function changeSlideLeft(){
-		carouselList.animate({'marginLeft':+600}, 500, moveLeft);
+		carouselList.animate({'marginLeft':0}, 500, moveLeft);
 	}
 
-	function move(){
+	function moveRight(){
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem)
-		carouselList.css({marginLeft:0});
+		carouselList.css({marginLeft:-600});
+		i++;
+		checkPoint();
 	}
 
 	function moveLeft(){
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
 		firstItem.before(lastItem)
-		carouselList.css({marginLeft:0});
+		carouselList.css({marginLeft:-600});
+		i--;
+		checkPoint();
 	}
 
-	$('.fa-chevron-right').on('click', function() {
-			changeSlide()
+	function resetInterval() {
+		clearInterval(interval);
+		interval = setInterval(changeSlideRight, 5000);
+	}
+
+	$('.fa-chevron-right').on('click', function () {
+		changeSlideRight();
+		resetInterval();
 	})
 
 	$('.fa-chevron-left').on('click', function() {
-			changeSlideLeft()
+		changeSlideLeft();
+		resetInterval();
 	})
 });	
 
